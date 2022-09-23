4c681ca65a0735f69c5e41e4e8e97494ba606451
Anna Ira <annairahurnaus@mytum.de>
2022-09-23 17:32:00 +0200

    empty Spring project

---
+++ src/main/java/org/atlat/complaintanalyzer/ComplaintAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package org.atlat.complaintanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplaintAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/atlat/complaintanalyzer/ComplaintAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.atlat.complaintanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
